94d61b375ea5570e9dbc11f8cdf26f2222d6a7fd
Ahmad Mustafa <ahmadmustafaansari@gmail.com>
2022-12-05 13:34:34 +0500

    9th

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,22 +1,76 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
+import authService from './authService'
+
+// Get user from LOCAL STORAGE
+const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
-  user: null,
+  user: user ? user : null,
   isError: false,
   isSuccess: false,
   isLoading: false,
   message: ''
 }
 
+// REGISTER a new user
+export const register = createAsyncThunk(
+  // action type string
+  'auth/register',
+  // callback function (referred to as a payloadCreator)
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user)
+    } catch (error) {
+      const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+ )
+ 
+ // LOGIN a user
+ export const login = createAsyncThunk(
+  // action type string
+  'auth/login',
+  // callback function (referred to as a payloadCreator)
+  async (user, thunkAPI) => {
+    console.log(user)
+  }
+ )
+
+
+// THE AUTH SLICE
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {},
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false
+      state.isError = false
+      state.isSuccess = false
+      state.message = ''
+    }
+  },
   extraReducers: (builder) => { 
-  
-  // to work with different scenarios like , registeration pending / or successfull
+    builder
+      .addCase(register.pending, (state) => { 
+        state.isLoading = true
+       })
+      .addCase(register.fulfilled, (state, action) => { 
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+       })
+      .addCase(register.rejected, (state, action) => { 
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        state.user = null
+       })
   }
-
 })
 
+// no doubt the redux code is so wierd and make no sense
+export const {reset} = authSlice.actions
 export default authSlice.reducer
+// or 
+// export const authReducer = authSlice.reducer
